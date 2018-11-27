0ca616a128c16b6983109b788727842bc97629b3
Mountain Xiang <783600983@qq.com>
2018-11-27 15:26:56 +0800

    resume AuthenticEnglish Resources

---
+++ AuthenticEnglish/skin/m/showlink.js
@@ -0,0 +1,7 @@
+document.writeln("<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>");
+document.writeln("<script>");
+document.writeln("  (adsbygoogle = window.adsbygoogle || []).push({");
+document.writeln("    google_ad_client: \"ca-pub-1903766104757163\",");
+document.writeln("    enable_page_level_ads: true");
+document.writeln("  });");
+document.writeln("</script>");
